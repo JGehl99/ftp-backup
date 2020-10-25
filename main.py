dc87b9ad75a6ae6d22acddffb878a6746ce7b5e5
Mona Lisa <jgehl99@gmail.com>
2020-10-24 20:54:05 -0400

    Removed unnecessary functions, cleaned up code

--- main.py
+++ main.py
@@ -1,74 +1,19 @@
 import ftplib
+from time import sleep
+
 import ftputil.session
 from config import config
 import os
 from zipfile import ZipFile
 from datetime import datetime
 import re
+import sys
 
-
-# Recursively calculates the total size of the given directory**
-def get_size(path, host):
-    total_bytes = 0
-    for filename in host.listdir(path):
-        filepath = host.path.join(path, filename)
-        if host.path.isfile(filepath):
-            total_bytes += host.path.getsize(filepath)
-        elif host.path.isdir(filepath):
-            total_bytes += get_size(filepath, host)
-    return total_bytes
-
-
-# Recursively downloads files from a given directory and stores them in the user's temp folder
-def download(path, host, temp):
-    for filename in host.listdir(path):
-        if host.path.isfile(host.path.join(path, filename)):
-            print("\nFile: ", host.path.join(path, filename))
-            if host.download_if_newer(host.path.join(path, filename), os.path.join(temp, filename)):
-                print("| ", filename, " | ", " Download Successful")
-            else:
-                print("| ", filename, " | ", " Download Failed ")
-        elif host.path.isdir(host.path.join(path, filename)):
-            if not filename.startswith("@"):
-                new_temp = os.path.join(temp, filename)
-                print("\nDirectory: ", host.path.join(path, filename))
-                if not os.path.exists(new_temp):
-                    os.mkdir(new_temp)
-                download(host.path.join(path, filename), host, new_temp)
-    return True
-
-
-# Takes in a list of paths and downloads each file to the user's temp folder, folder structure is retained
-def download_from_paths(host, temp):
-    with open(os.path.join(temp_dir, "files.txt"), 'r') as file:
-        for filepath in file.read().splitlines():
-            print("Downloading ", filepath)
-            if not os.path.exists(temp + re.sub("/", r"\\", os.path.split(filepath)[0])):
-                os.makedirs(temp + re.sub("/", r"\\", os.path.split(filepath)[0]))
-            if host.download_if_newer(filepath, temp + re.sub("/", r"\\", filepath)):
-                print("Success")
-            else:
-                print("FAIL")
-
-
-# Returns absolute directory for all files in given path
-def get_paths(path):
-    file_paths = []
-    for root, directories, files in os.walk(path):
-        for filename in files:
-            filepath = os.path.join(root, filename)
-            file_paths.append(filepath)
-    return file_paths
-
-
-# Returns absolute directory for all files in given ftp remote path
-def get_remote_paths(path, host):
-    file_paths = []
-    for root, directories, files in host.walk(path):
-        for filename in files:
-            filepath = host.path.join(root, filename)
-            file_paths.append(filepath)
-    return file_paths
+# Debug enable
+if len(sys.argv) > 1:
+    debug_ftp = int(sys.argv[1])
+else:
+    debug_ftp = 0
 
 
 # Create temp_dir
@@ -83,17 +28,34 @@ if not os.path.exists(dest_dir):
 
 # Setting up FTP settings
 session_factory = ftputil.session.session_factory(base_class=ftplib.FTP_TLS, port=config["port"],
-                                                  encrypt_data_channel=True, debug_level=0)
+                                                  encrypt_data_channel=True, debug_level=debug_ftp)
 # Connect to FTP server
 ftp_host = ftputil.FTPHost(config["host"], config["user"], config["passwd"], session_factory=session_factory)
 
-with open(os.path.join(temp_dir, "files.txt"), 'w') as f:
-    for path in get_remote_paths(dest_dir, ftp_host):
-        f.write(path + "\n")
-
-download_from_paths(ftp_host, temp_dir)
+for root, directories, files in ftp_host.walk(config["dl_dir"]):
+    for filename in files:
+        filepath = ftp_host.path.join(root, filename)
+        if debug_ftp > 0:
+            print("\nDownloading ", filepath)
+        if not os.path.exists(temp_dir + re.sub("/", r"\\", os.path.split(filepath)[0])):
+            os.makedirs(temp_dir + re.sub("/", r"\\", os.path.split(filepath)[0]))
+        if ftp_host.download_if_newer(filepath, temp_dir + re.sub("/", r"\\", filepath)):
+            if debug_ftp > 0:
+                print("Success")
+        else:
+            if debug_ftp > 0:
+                print("FAIL")
 
 # Zip backed up files
 with ZipFile(os.path.join(config["path"], "backup-" + datetime.now().strftime("%y%m%d-%H-%M-%S") + ".zip"), "w") as zipfile:
-    for file in get_paths(temp_dir):
-        zipfile.write(file, arcname=re.search(r'ftp-backup\\(.*)', file).group(1))
+    for root, directories, files in os.walk(temp_dir):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            zipfile.write(filepath, arcname=re.search(r'ftp-backup\\(.*)', filepath).group(1))
+
+temp_dir_list = os.listdir(config["path"])
+backup_count = len(temp_dir_list)
+
+if backup_count - 10 > 2:
+    for x in range(0, backup_count - 10, 1):
+        os.remove(os.path.join(config["path"], temp_dir_list[x]))
